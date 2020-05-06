e87085a116692f9b405e65a103675b1c4aa274b7
victorismaelreeves <50830454+victorismaelreeves@users.noreply.github.com>
2020-05-07 01:54:23 +0200

    Update main.go

--- main.go
+++ main.go
@@ -13,16 +13,21 @@ type Simple struct {
 	Url string
 }
 
+func SimpleFactory (host string) Simple {
+	return Simple{"Hello", "World", host}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	simple := Simple{"Hello", "World",r.Host}
+	simple := SimpleFactory(r.Host)
 
 	jsonOutput, _ := json.Marshal(simple)
 
 	fmt.Fprintln(w, string(jsonOutput))
+	// fmt.Fprintln(w, simple)
 }
 
 func main() {
-	fmt.Println("Server started")
+	fmt.Println("Server started normally")
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
