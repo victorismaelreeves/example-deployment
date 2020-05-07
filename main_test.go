4207267ca492bdd39c49f56150871ada6237a836
victorismaelreeves <50830454+victorismaelreeves@users.noreply.github.com>
2020-05-07 02:01:18 +0200

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,1 +1,12 @@
 
+package main
+
+import "testing"
+
+func TestSimpleFactory(t *testing.T) {
+	f := SimpleFactory("http://localhost")
+
+	if f.Url != "http://localhost" {
+		t.Errorf("feature incorrect, got %s, want: %s ", f.Url, "http://localhost")
+	}
+}
